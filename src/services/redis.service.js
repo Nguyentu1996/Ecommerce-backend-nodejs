b2302c1ebca50ef73dedf1827b462d15e00d58eb
nguyentu <tun@bap.jp>
2024-11-19 11:49:13 +0700

    Update ratelimiter

--- src/services/redis.service.js
+++ src/services/redis.service.js
@@ -5,20 +5,28 @@ const { getRedis } = require("../dbs/init.redis");
 const redisClient = getRedis()
 
 const setCache = async (key, value) => {
-    await redisClient.set(key, value);
+    await redisClient.instanceConnect.set(key, value);
 }
 
 const setListCache = async (key, value = []) => {
-    await redisClient.rPush(key, JSON.stringify(value)); // 60 minutes
+    await redisClient.instanceConnect.rPush(key, JSON.stringify(value));
+}
+
+const increaseCache = async (key) => {
+    return await redisClient.instanceConnect.incr(key);
+}
+
+const setExpireCache = async (key, time) => {
+    await redisClient.instanceConnect.pExpire(key, Number(time))
 }
 
 const getListCache = async (key) => {
-    const listItems = await redisClient.lRange(key, 0, -1);
+    const listItems = await redisClient.instanceConnect.lRange(key, 0, -1);
     return listItems.map(item => JSON.parse(item));
 }
 
 const getCache = async (key) => {
-    return await redisClient.get(key);
+    return await redisClient.instanceConnect.get(key);
 }
 
 module.exports = {
@@ -26,4 +34,6 @@ module.exports = {
     getCache,
     setListCache,
     getListCache,
+    increaseCache,
+    setExpireCache
 }
